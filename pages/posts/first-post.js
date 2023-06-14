ea4522f310f8de5205234b81b98755f4458f91b7
hakraj <adekunleddream@gmail.com>
2023-06-14 21:46:54 +0100

    deploying blog app on vercel

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+
+const FirstPost = () => {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+export default FirstPost;
